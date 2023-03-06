511a8af82c22982030aa2a9447b3364143e94612
FloydP1nk <113801739+FloydP1nk@users.noreply.github.com>
2023-03-06 23:14:33 +0300

    improve code style

--- hw.cpp
+++ hw.cpp
@@ -1,8 +1,7 @@
 #include "iostream"
 #include "string"
-using namespace std;
 int main() {
-    string userName;
-    cin >> userName;
-    cout << "Hello world from " << userName << endl;
+    std::string userName;
+    std::cin >> userName;
+    std::cout << "Hello world from " << userName << std::endl;
 }
